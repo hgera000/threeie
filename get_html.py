153f85d5e8adcb5475e35adc8a6573d26ae3c596
hgera000 <hgerard@poverty-action.org>
2014-07-30 16:21:50 -0400

    Source now a single entry and all 6 types of papers tested and working.

--- get_html.py
+++ get_html.py
@@ -1,9 +1,9 @@
 import requests,io,pdb
 
-save_folder = "/home/hgera000/threeie/html/"
+save_folder = "/home/hgerard/threeie/html/"
 base_url = "http://www.3ieimpact.org/en/evidence/impact-evaluations/details/"
 
-for pid in range(1,3):
+for pid in range(247,248):
     #pdb.set_trace()
     html = requests.get("%s%d" % (base_url,pid))
     with io.open("%s%d.html" % (save_folder,pid), 'w', encoding='utf8') as f:
--- parse_html.py
+++ parse_html.py
@@ -4,29 +4,23 @@ import os,pdb,re,io
 from collections import defaultdict
 
 
-source_folder = "/home/hgera000/threeie/html/"
-studies = io.open('/home/hgera000/threeie/data/studies.txt','w',encoding='utf8')
-authors = io.open('/home/hgera000/threeie/data/authors.txt','w',encoding='utf8')
-sectors = io.open('/home/hgera000/threeie/data/sectors.txt','w',encoding='utf8')
-subgroups = io.open('/home/hgera000/threeie/data/subgroups.txt','w',encoding='utf8')
-designs = io.open('/home/hgera000/threeie/data/designs.txt','w',encoding='utf8')
+source_folder = "/home/hgerard/threeie/html/"
+studies = io.open('/home/hgerard/threeie/data/studies.txt','w',encoding='utf8')
+authors = io.open('/home/hgerard/threeie/data/authors.txt','w',encoding='utf8')
+sectors = io.open('/home/hgerard/threeie/data/sectors.txt','w',encoding='utf8')
+subgroups = io.open('/home/hgerard/threeie/data/subgroups.txt','w',encoding='utf8')
+designs = io.open('/home/hgerard/threeie/data/designs.txt','w',encoding='utf8')
 
 
-for fname in sorted(os.listdir(source_folder)):
+for fname in sorted(os.listdir(source_folder),key=lambda x: int(x.split(".")[0])):
     d = defaultdict(list); #new data dictionary
     print ('Parsing file named %s' % fname)
     with open("%s%s" % (source_folder,fname),'r') as f:
         soup = BeautifulSoup(f.read())
         d['title'] = [soup.article.h1.text.strip(),]
         pdb.set_trace()
-        d['source'] = soup.find('h2',text=re.compile('Publication Source')).findNext('p',text=re.compile('Available From:')).rstrip('Available From:').strip().split(', ')
-        if len(d['source'])==2:
-            d['source'] = d['source']+["NULL","NULL","NULL"]
-        if len(d['source'])==3:
-            d['source'] = d['source']+["NULL","NULL",]
-        if len(d['source'])==4:
-            d['source'] = d['source']+["NULL",]
-
+        d['year'] = re.sub(re.compile('[^0-9]'),'',soup.time.text)
+        d['source'] = re.sub(re.compile('Available From:$',re.IGNORECASE),'',soup.find('h2',text=re.compile('Publication Source')).findNext('p',text=re.compile('Available', re.IGNORECASE)).strip())
         
         d['link'] = [soup.find('a',{'class':"button evidence_link"})['href'].strip(),]
         meta = soup.find('section', {'class': 'evidence_meta'})
@@ -37,7 +31,7 @@ for fname in sorted(os.listdir(source_folder)):
             if dt[i].text == 'Authors':
                 d[dt[i].text.lower().strip()] = re.split(',| and ', dd[i].text.strip())
             else:
-                d[dt[i].text.lower().strip()] = dd[i].text.strip().split(', ')
+                d[dt[i].text.lower().strip()] = dd[i].text.strip().replace('&amp;', '&').split(', ')
 
         details = soup.findAll('section',{'class': 'summary_item'})
         for det in details:
@@ -50,23 +44,23 @@ for fname in sorted(os.listdir(source_folder)):
         if 'about this impact evaluation' not in d:
             d['about this impact evaluation'] = "NULL"
 
-        stdout1 = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s";
+        stdout1 = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s";
 
-        print(stdout1 % (fname.strip('.html'),d['title'][0],d['status'][0],d['country'][0],d['region'][0],d['source'][0],d['source'][1],";".join(d['source'][2:]),d['link'][0],d['gender focus'][0], d['methodology'], d['main findings'], d['about this impact evaluation']),file=studies)
+        print(stdout1 % (fname.rstrip('.html'),d['title'][0],d['status'][0],d['year'],d['country'][0],d['region'][0],d['source'],d['link'][0],d['gender focus'][0], d['methodology'], d['main findings'], d['about this impact evaluation']),file=studies)
 
         stdout2 = "%s|%s";
 
         for l in d['authors']:
-            print(stdout2 % (fname.strip('.html'),l),file=authors)
+            print(stdout2 % (fname.rstrip('.html'),l),file=authors)
 
         for l in d['sector']:
-            print(stdout2 % (fname.strip('.html'),l),file=sectors)
+            print(stdout2 % (fname.rstrip('.html'),l),file=sectors)
 
         for l in d['selected sub-groups']:
-            print(stdout2 % (fname.strip('.html'),l),file=subgroups)
+            print(stdout2 % (fname.rstrip('.html'),l),file=subgroups)
 
         for l in d['evaluation designs']:
-            print(stdout2 % (fname.strip('.html'),l),file=designs)
+            print(stdout2 % (fname.rstrip('.html'),l),file=designs)
 
 authors.close()
 sectors.close()
